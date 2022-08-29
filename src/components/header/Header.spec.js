d55ed32a957ae8070d81d18dcb582b2d630b693a
Alucard163 <romemix163@gmail.com>
2022-08-29 15:24:27 +0300

    add some tests

--- src/components/header/Header.spec.js
+++ src/components/header/Header.spec.js
@@ -3,29 +3,13 @@ import Header from "./Header";
 import { render, screen } from "@testing-library/react";
 
 describe('Header component', () => {
-    it.skip('check login page', () => {
-        render(<Header page="login"/>);
+    it('check render', () => {
+        render(<Header/>);
         const imgExpect = '<img src="logo.svg" alt="логотип такси" class="logo">'
 
         expect(screen.getByTestId('header-component').innerHTML).toContain(imgExpect)
-        console.log(screen.getByTestId('header-component').innerHTML);
-    })
-
-    it.skip('check register page', () => {
-        render(<Header page="register"/>)
-
-        console.log(screen.getByTestId('header-component').innerHTML);
-    })
-
-    it.skip('check map page', () => {
-        render(<Header page="map"/>)
-
-        console.log(screen.getByTestId('header-component').innerHTML);
-    })
-
-    it.skip('check profile page', () => {
-        render(<Header page="profile"/>)
-
-        console.log(screen.getByTestId('header-component').innerHTML);
+        expect(screen.getByText('Карта')).toBeInTheDocument();
+        expect(screen.getByText('Профиль')).toBeInTheDocument();
+        expect(screen.getByText('Выйти')).toBeInTheDocument();
     })
 })
---
+++ src/context/AuthContext.spec.js
@@ -0,0 +1,25 @@
+import {render, screen} from '@testing-library/react'
+import { AuthContext } from "./AuthContext";
+
+const customRender = (ui, {providerProps, ...renderOptions}) => {
+    const isLoggedIn = true
+    return render(
+        <AuthContext.Provider value={isLoggedIn} {...providerProps}>{ui}</AuthContext.Provider>,
+        renderOptions,
+    )
+}
+
+describe('AuthContext', () => {
+    it('check auth context', () => {
+        const providerProps = {
+            isLoggedIn: 'true',
+        }
+        customRender(
+            <AuthContext.Consumer>
+                {(value) => <span>Received: {String(value)}</span>}
+            </AuthContext.Consumer>,
+            {providerProps},
+        )
+        expect(screen.getByText(/^Received:/).textContent).toBe('Received: true')
+    })
+})
